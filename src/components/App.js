98cb21d753d50d2e7f5058c5d3fddc207a05975a
taye000 <taylorgitari@gmail.com>
2022-01-19 18:55:20 +0300

    contact manager saving to local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+  
   return (
     <div className="ui container">
       <Header />
