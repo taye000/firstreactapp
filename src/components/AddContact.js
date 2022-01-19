c36f170e7d055e58856a76fbb9b9e4b3f8a049ff
taye000 <taylorgitari@gmail.com>
2022-01-19 18:33:27 +0300

    contact manager

---
+++ src/components/AddContact.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+class AddContact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      phone: "",
+      email: "",
+    };
+  }
+
+  add = (e) => {
+    e.preventDefault();
+    if (
+      this.state.fname === "" ||
+      this.state.lname === "" ||
+      this.state.phone === "" ||
+      this.state.email === ""
+    ) {
+      alert("All the fields are required MF");
+      return;
+    }
+    this.props.addContactHandler(this.state);
+    console.log(this.state);
+    this.setState({ fname: "", lname: "", phone: "", email: "" });
+  };
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form" onSubmit={this.add}>
+          <div className="field">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="fname"
+              placeholder="First Name"
+              value={this.state.fname}
+              onChange={(e) => this.setState({ fname: e.target.value })}
+            ></input>
+          </div>
+          <div className="field">
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lname"
+              placeholder="Last Name"
+              value={this.state.lname}
+              onChange={(e) => this.setState({ lname: e.target.value })}
+            ></input>
+          </div>
+          <div className="field">
+            <label>Phone Number</label>
+            <input
+              type="number"
+              name="phone"
+              placeholder="Phone Number"
+              value={this.state.phone}
+              onChange={(e) => this.setState({ phone: e.target.value })}
+            ></input>
+          </div>
+          <div className="field">
+            <label>Email Address</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            ></input>
+          </div>
+          <button className="ui button blue">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import user from "../images/user.png";
+
+const ContactCard = (props) => {
+  const { fname, lname, phone, email } = props.contact;
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
+      <div className="content">
+        <div className="header">{fname}</div>
+        <div className="header">{lname}</div>
+        <div>{phone}</div>
+        <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+      ></i>
+    </div>
+  );
+};
+
+export default ContactCard;
